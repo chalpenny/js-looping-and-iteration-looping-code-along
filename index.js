6954030988c72c2eb1c523d2a8605661969dd488
chalpenny <cp.halpenny@gmail.com>
2020-02-11 23:35:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const occasion = ['surprise'];
+
+function writeCards(names, occasion) {
+    let thankYouCards = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return thankYouCards;
+}
+
+
+
+
+
+function countDown(number) {
+    while (0 <= number) {
+      console.log(number--);
+    }
+  }
+  
+  countDown(10);
